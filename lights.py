e89ae5fcbe281e8fd035c768d55a6cc83c96abcf
amaclean2 <andrew.n.maclean@gmail.com>
2021-04-28 13:22:58 -0700

    lights

--- lights.py
+++ lights.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+while True :
+    led.on()
+    print("On")
+    sleep(1)
+    led.off()
+    print("Off")
