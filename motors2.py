bf9b43bd09273e733903fa5283b6d6c4a4ac77d7
amaclean2 <andrew.n.maclean@gmail.com>
2021-07-22 00:16:46 -0700

    added more logging

--- motors2.py
+++ motors2.py
@@ -11,15 +11,16 @@ ESC_GPIO = 13
 
 # calibrating the ESC
 pi.set_servo_pulsewidth(ESC_GPIO, 2000)
-print("testing_hi")
+print("calibrate_hi")
 sleep(2)
 pi.set_servo_pulsewidth(ESC_GPIO, 1000)
-print("testing_lo")
+print("calibrate_lo")
 sleep(2)
 
 # running the esc at 6 different speeds
 for speed in range(6) :
     pi.set_servo_pulsewidth(ESC_GPIO, speed * 1000 / 7 + 1000)
+    print("testing speed " + speed)
     sleep(2)
 
 # shutting everything down
