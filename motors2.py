b4ab7dc16b2fbc26ff2b81c697d9768de61108c9
amaclean2 <andrew.n.maclean@gmail.com>
2021-07-22 00:13:54 -0700

    adding logs

--- motors2.py
+++ motors2.py
@@ -8,14 +8,20 @@ GPIO.setwarnings(False)
 pi = pigpio.pi()
 
 ESC_GPIO = 13
+
+# calibrating the ESC
 pi.set_servo_pulsewidth(ESC_GPIO, 2000)
+print("testing_hi")
 sleep(2)
 pi.set_servo_pulsewidth(ESC_GPIO, 1000)
+print("testing_lo")
 sleep(2)
 
+# running the esc at 6 different speeds
 for speed in range(6) :
     pi.set_servo_pulsewidth(ESC_GPIO, speed * 1000 / 7 + 1000)
     sleep(2)
 
+# shutting everything down
 pi.set_servo_pulsewidth(ESC_GPIO, 0)
 pi.stop()
